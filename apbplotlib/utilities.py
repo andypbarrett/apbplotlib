9538cdf4c14274ff7eb8a9f592a414e06a6f1446
Andy Barrett <apbarret@nsidc.org>
2018-11-02 15:58:27 -0600

    added imports of matplotlib.colors and numpy

--- apbplotlib/utilities.py
+++ apbplotlib/utilities.py
@@ -1,6 +1,8 @@
+import matplotlib.colors as mcolors
+import numpy as np
 
 # set the colormap and centre the colorbar
-class MidpointNormalize(colors.Normalize):
+class MidpointNormalize(mcolors.Normalize):
     """
     Normalise the colorbar so that diverging bars work there way either 
     side from a prescribed midpoint value)
@@ -9,7 +11,7 @@ class MidpointNormalize(colors.Normalize):
     """
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
         self.midpoint = midpoint
-        colors.Normalize.__init__(self, vmin, vmax, clip)
+        mcolors.Normalize.__init__(self, vmin, vmax, clip)
 
     def __call__(self, value, clip=None):
         # I'm ignoring masked values and all kinds of edge cases to make a
