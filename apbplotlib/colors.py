3fa70ea52e363046ccc09334106686f74a5e8e60
Andy Barrett <apbarret@nsidc.org>
2019-04-01 16:32:18 -0600

    created color table module with reanalysis colors

---
+++ apbplotlib/colors.py
@@ -0,0 +1,25 @@
+# Utilities and color maps for plotting
+
+reanalysis_color = {
+    'MERRA2'  :'#e21f26',
+    'MERRA'   :'#f69999',
+    'ERAI'    :'#295f8a',
+    'ERA5'    :'#5f98c6',
+    'ERA40'   :'#afcbe3',
+    'JRA55'   :'#723b7a',
+    'JRA55C'  :'#ad71b5',
+    'JRA25'   :'#d6b8da',
+    'NCEP1'   :'#f57e20',
+    'NCEP2'   :'#fdbf6e',
+    '20CRV2C' :'#ec008c',
+    '20CRV2'  :'#f799D1',
+    'CERA20C' :'#00aeef',
+    'ERA20C'  :'#60c8e8',
+    'CFSR'    :'#34a048',
+    'REM'     :'#b35b28', # reanalysis ensemble mean
+    'OTHER'   :'#ffd700',
+    'OBS'     :'#000000', # observations black
+    'OBS2'    :'#777777',
+} # observations grey
+
+
