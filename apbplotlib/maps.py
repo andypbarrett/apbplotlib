6e37230ce2dd371c026bbeb9d973335f972e3aa2
Andy Barrett <apbarret@nsidc.org>
2020-04-29 12:16:45 -0600

    add EASE-Grid 2.0 North definition

--- apbplotlib/maps.py
+++ apbplotlib/maps.py
@@ -1,7 +1,57 @@
 import cartopy.crs as ccrs
 
 
-# projection class
+class EASE2_North(ccrs.Projection):
+    """
+    Projection class for NSIDC EASE-Grid 2.0 North
+
+    See Brodzik, M. J., B. Billingsley, T. Haran, B. Raup, M. H. Savoie. 2014. EASE-Grid 2.0: Incremental but Significant Improvements for Earth-Gridded Data Sets. ISPRS International Journal of Geo-Information 2012, 1, 32-45 & correction in 2014, 3, 1154-1156.
+
+    Uses the default Globe object which is WGS84
+    """
+
+    def __init__(self):
+
+        proj4_params = {
+            'proj': 'laea',
+            'lat_0': 90.,
+            'lon_0': 0.,
+            'x_0': 0,
+            'y_0': 0,
+            'ellps': 'WGS84',
+            #'towgs84': 0,0,0,0,0,0,0,
+            'units': 'm',
+            'no_defs': ''
+            }
+
+        super(EASE2_North, self).__init__(proj4_params)
+
+    @property
+    def boundary(self):
+        coords = ((self.x_limits[0], self.y_limits[0]),(self.x_limits[1], self.y_limits[0]),
+                  (self.x_limits[1], self.y_limits[1]),(self.x_limits[0], self.y_limits[1]),
+                  (self.x_limits[0], self.y_limits[0]))
+
+        return ccrs.sgeom.Polygon(coords).exterior
+
+    @property
+    def threshold(self):
+        return 1e5
+
+    @property
+    def x_limits(self):
+        return (None, None)
+
+    @property
+    def y_limits(self):
+        return (None, None)
+
+    def describe(self):
+        for k, v in vars(self).items():
+            print (f'{k}: {v}')
+
+            
+# Original EASE North projection class
 class EASE_North(ccrs.Projection):
     '''Projection class for NSIDC EASE grid north'''
     
